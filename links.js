363934cd56eb169d058a3f4bd29e1bd27e4a2303
Sirni4ek <128617290+Sirni4ek@users.noreply.github.com>
2024-05-08 17:48:35 +0300

    Update links.js

--- links.js
+++ links.js
@@ -4,13 +4,13 @@ function insertCommonLinks() {
   linkContainer.classList.add("common-links"); // Add a class for styling
 
   // Define the links as an object with link text and URL
-  const links = {
+  const links = [
     { text: "Dima's Page", url: "https://sirni4ek.github.io/" },
     { text: "Danya's Page", url: "https://danil357345.github.io/" },
     { text: "Solomiia's Page", url: "https://solomiia2098.github.io/" },
     { text: "Ilya's Page", url: "https://gulkily.github.io/" }
     
- };
+ ];
 
   // Loop through the links object and create anchor elements
   for (const linkText in links) {
