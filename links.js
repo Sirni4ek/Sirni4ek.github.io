9e295389b229775e0bc9c8b91164ebb91f03a03d
Ilya Gulko <1530699+gulkily@users.noreply.github.com>
2024-05-08 11:45:07 -0400

    Update links.js - fix issue with link captions and href

--- links.js
+++ links.js
@@ -3,20 +3,20 @@ function insertCommonLinks() {
   const linkContainer = document.createElement("nav");
   linkContainer.classList.add("common-links"); // Add a class for styling
 
-  // Define the links as an array of objects with link text and URL
+  // Define the links as an object with link text and URL
   const links = [
     { text: "Dima's Page", url: "https://sirni4ek.github.io/" },
     { text: "Danya's Page", url: "https://danil357345.github.io/" },
     { text: "Solomiia's Page", url: "https://solomiia2098.github.io/" },
-    { text: "Ilya's Page", url: "https://gulkily.github.io/" },
+    { text: "Ilya's Page", url: "https://gulkily.github.io/" }
   ];
 
-  // Loop through the links array and create anchor elements
-  for (let i = 0; i < links.length; i++) {
-    const link = document.createElement("a");
-    link.textContent = links[i].text;
-    link.href = links[i].url;
-    linkContainer.appendChild(link);
+  // Loop through the links object and create anchor elements
+  for (const link of links) {
+    const linkElement = document.createElement("a");
+    linkElement.textContent = link.text;
+    linkElement.href = link.url;
+    linkContainer.appendChild(linkElement);
   }
 
   // Get the body element
